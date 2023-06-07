9831cc203a012ab9fad8ee570abf5c0187e83eae
João Lemos <131999007+JoaoLemosSys@users.noreply.github.com>
2023-06-07 16:25:48 -0300

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -1,7 +1,7 @@
 import socket
 import threading
 import rsa
-
+import base64
 
 class Server:
     def __init__(self, host, port):
@@ -25,39 +25,49 @@ class Server:
             encrypted_message_size_data = client.recv(4)
             encrypted_message_size = int.from_bytes(encrypted_message_size_data, byteorder='big')
 
+
             # Receber a mensagem criptografada
             encrypted_message = client.recv(encrypted_message_size)
 
-            # Descriptografar a mensagem usando a chave privada
             message = encrypted_message.decode('utf-8')
-            print("message:" + message)
-
-            if ':' in message:
-                # A mensagem contém um nome de usuário de destino
-                destination_nickname, message = message.split(':', 1)
-                print("destination_nickname:" + destination_nickname)
-
-                if destination_nickname in self.public_keys:
-                    print("achou a chave")
-                    # Criptografar a mensagem usando a chave pública do usuário de destino
-                    public_key = self.public_keys[destination_nickname]
-                    encrypted_message = rsa.encrypt(message.encode('utf-8'), public_key)
-
-                    # Enviar o tamanho da mensagem criptografada em bytes
-                    encrypted_message_size_data = len(encrypted_message).to_bytes(4, byteorder='big')
-                    print("tamanho da mensagem enviada")
-                    print(encrypted_message_size_data)
-
-                    # Enviar a mensagem criptografada ao usuário de destino
-                    destination_client = self.nicknames[destination_nickname]
-                    destination_client.send(encrypted_message_size_data + encrypted_message)
-                else:
-                    # Usuário de destino não encontrado
-                    error_message = f'ERROR: Usuário {destination_nickname} não encontrado'
-                    client.send(error_message.encode('utf-8'))
+            if message.startswith('PUBLIC_KEY_REQUEST:'):
+                # A mensagem é uma solicitação de chave pública
+                sender_nickname = message.split(':', 1)[1]
+                self.send_public_key(client, sender_nickname)
             else:
-                # A mensagem não contém um nome de usuário de destino
-                self.broadcast(client, message.encode('utf-8'))
+                if ':' in message:
+                    # A mensagem contém um nome de usuário de destino
+                    destination_nickname, message = message.split(':', 1)
+                    print("destination_nickname:" + destination_nickname)
+
+                    if destination_nickname in self.public_keys:
+
+                        # Enviar o tamanho da mensagem criptografada em bytes
+                        encrypted_message_size_data = len(encrypted_message).to_bytes(4, byteorder='big')
+
+                        # Enviar a mensagem criptografada ao usuário de destino
+                        destination_client = self.nicknames[destination_nickname]
+                        destination_client.send(encrypted_message_size_data + encrypted_message)
+                    else:
+                        # Usuário de destino não encontrado
+                        error_message = f'ERROR: Usuário {destination_nickname} não encontrado'
+                        client.send(error_message.encode('utf-8'))
+                else:
+                    # A mensagem não contém um nome de usuário de destino
+                    self.broadcast(client, message.encode('utf-8'))
+
+    def send_public_key(self, client, sender_nickname):
+        if sender_nickname in self.public_keys:
+            # Enviar a chave pública do remetente ao cliente solicitante
+            public_key_data = self.public_keys[sender_nickname].save_pkcs1()
+            full_message_public_key = f'{sender_nickname}:{public_key_data}'
+
+            public_key_size_data = len(full_message_public_key).to_bytes(4, byteorder='big')
+            client.send(public_key_size_data + full_message_public_key.encode('utf-8'))
+        else:
+            # Remetente não encontrado
+            error_message = f'ERROR: Usuário {sender_nickname} não encontrado'
+            client.send(error_message.encode('utf-8'))
 
     def receive_public_key(self, client):
         # Receber o tamanho da chave pública em bytes
