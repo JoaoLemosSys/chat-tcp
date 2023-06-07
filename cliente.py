1626d200494bd8d543ca248b97435096ce7a67a6
João Lemos <131999007+JoaoLemosSys@users.noreply.github.com>
2023-06-07 16:26:46 -0300

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -1,3 +1,4 @@
+import base64
 import socket
 import threading
 import rsa
@@ -13,6 +14,9 @@ class Client:
         # Gerar par de chaves RSA
         (self.public_key, self.private_key) = rsa.newkeys(2048)
 
+        # Dicionário para armazenar as chaves públicas dos outros usuários
+        self.other_public_keys = {}
+
     def connect(self):
         self.client.connect((self.host, self.port))
 
@@ -24,30 +28,70 @@ class Client:
         # Enviar apelido ao servidor
         self.client.send(self.nickname.encode('utf-8'))
 
-    def send_message(self, message):
+    def send_message(self, message, _nickname):
 
-        tamanho_da_messagem = len(message).to_bytes(4, byteorder='big')
-        self.client.send(tamanho_da_messagem+message.encode('utf-8'))
+            if _nickname in self.other_public_keys:
+                public_key = self.other_public_keys[_nickname]
+            else:
+                 self.request_public_key(_nickname)
+                 public_key = self.other_public_keys[_nickname]
+
+            # Criptografar a mensagem usando a chave privada do cliente
+            encrypted_message = rsa.encrypt(message.encode('utf-8'), public_key)
+
+            full_message = f'{_nickname}:{encrypted_message}'
+
+            # Enviar o tamanho da mensagem criptografada em bytes
+            full_message_size_data = len(full_message).to_bytes(4, byteorder='big')
+
+            self.client.send(full_message_size_data + full_message.encode('utf-8'))
+
+
+    def request_public_key(self, sender_nickname):
+
+        request_message = f'PUBLIC_KEY_REQUEST:{sender_nickname}'
+        request_message_size_data = len(request_message).to_bytes(4, byteorder='big')
+        self.client.send(request_message_size_data + request_message.encode('utf-8'))
+
+
+
 
     def receive_messages(self):
         while True:
-            # Receber o tamanho da mensagem criptografada em bytes
-            encrypted_message_size_data = self.client.recv(4)
-            print(encrypted_message_size_data.decode())
-            encrypted_message_size = int.from_bytes(encrypted_message_size_data, byteorder='big')
-
-            # Receber a mensagem criptografada
-
-            encrypted_message = self.client.recv(encrypted_message_size)
-
-
             try:
-                # Tentar descriptografar a mensagem usando a chave privada
-                decrypted_message = rsa.decrypt(encrypted_message, self.private_key).decode('utf-8')
-                print(f'{decrypted_message}')
+
+                received_message_size_data = self.client.recv(4)
+                received_message_size = int.from_bytes(received_message_size_data, byteorder='big')
+
+
+                received_message = self.client.recv(received_message_size)
+                str_received_message = received_message.decode('utf-8')
+
+                print(str_received_message)
+                if ':' in str_received_message:
+
+                    sender_nickname, encrypted_message = str_received_message.split(':', 1)
+
+                    if '-BEGIN RSA PUBLIC KEY-' in encrypted_message:
+
+
+                        print(sender_nickname)
+                        encrypted_message = encrypted_message[2:]
+                        print(encrypted_message)
+
+                        public_key = rsa.PublicKey.load_pkcs1(encrypted_message)
+                        print(public_key)
+
+                        # Armazenar a chave pública do remetente
+                      #  self.other_public_keys[sender_nickname] = public_key
+                    else:
+                        decrypted_message = rsa.decrypt(encrypted_message.encode('utf-8'), self.private_key).decode('utf-8')
+                        print(f'{decrypted_message}')
+                else:
+                    print(f"{'Usuario nao identificado. Falha'}")
             except rsa.DecryptionError:
                 # Mensagem não criptografada ou criptografada com outra chave
-                print(f'{encrypted_message.decode("utf-8")}')
+                print('Mensagem nao criptograda ou chave invalida.')
 
 
 if __name__ == '__main__':
@@ -61,6 +105,7 @@ if __name__ == '__main__':
     while True:
         destination_nickname = input('Digite o apelido do destinatário: ')
         message = input('Digite sua mensagem: ')
+        print(message)
 
-        full_message = f'{destination_nickname}:{message}'
-        client.send_message(full_message)
+        #full_message = f'{destination_nickname}:{message}'
+        client.send_message(message, nickname)
