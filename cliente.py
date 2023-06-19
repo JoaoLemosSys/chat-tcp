db1a05178d4b558e517e3dea3387ad2a8fc6ebea
Joao Lemos <joao.lemos.sys@gmail.com>
2023-06-19 13:56:34 -0300

    Comentarios

--- cliente.py
+++ cliente.py
@@ -4,95 +4,131 @@ import sys
 import threading
 import rsa
 
+# Define a classe Cliente para um cliente de chat
 class Client:
+    # Inicializa o cliente com host, porta e apelido
     def __init__(self, host, port, nickname):
-        self.host = host  # Endereço IP do servidor de chat
-        self.port = port  # Porta do servidor de chat
-        self.nickname = nickname  # Apelido do usuário
-        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Cria um objeto de soquete para se conectar ao servidor
-        (self.public_key, self.private_key) = rsa.newkeys(256)  # Gera um par de chaves pública/privada RSA
-        self.other_public_keys = {}  # Armazena as chaves públicas de outros usuários
+        self.host = host
+        self.port = port
+        self.nickname = nickname
+        # Cria um objeto de soquete para se conectar ao servidor
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Gera um par de chaves pública/privada para criptografia RSA
+        (self.public_key, self.private_key) = rsa.newkeys(256)
+        # Dicionário para armazenar chaves públicas de outros usuários
+        self.other_public_keys = {}
 
+    # Conecta o cliente ao servidor
     def connect(self):
-        self.client.connect((self.host, self.port))  # Conecta-se ao servidor de chat
+        self.client.connect((self.host, self.port))
 
-        public_key_data = self.public_key.save_pkcs1()  # Salva a chave pública em formato PKCS1
-        public_key_size_data = len(public_key_data).to_bytes(4, byteorder='big')  # Obtém o tamanho da chave pública em bytes
-        self.client.send(public_key_size_data + public_key_data)  # Envia o tamanho e a chave pública para o servidor
+        # Envia a chave pública do cliente para o servidor
+        public_key_data = self.public_key.save_pkcs1()
+        public_key_size_data = len(public_key_data).to_bytes(4, byteorder='big')
+        self.client.send(public_key_size_data + public_key_data)
 
-        self.client.send(self.nickname.encode('utf-8'))  # Envia o apelido para o servidor
+        # Envia o apelido do cliente para o servidor
+        self.client.send(self.nickname.encode('utf-8'))
 
+    # Desconecta o cliente do servidor
     def disconnect(self):
-        self.client.close()  # Fecha a conexão com o servidor
+        self.client.close()
 
+    # Envia uma mensagem criptografada para outro usuário
     def send_message(self, message, destination_nickname):
         try:
+            # Verifica se a chave pública do destinatário já foi recebida
             if destination_nickname in self.other_public_keys:
-                public_key = self.other_public_keys[destination_nickname]  # Obtém a chave pública do destinatário se já estiver armazenada localmente
+                public_key = self.other_public_keys[destination_nickname]
             else:
+                # Se a chave pública do destinatário ainda não foi recebida, solicita a chave pública do destinatário
                 print(f'\nNão foi possivel enviar mensagem para {destination_nickname}.\nUsuario ainda não possui chave cadastrada. Solicitando chave... \n')
-                self.request_public_key(destination_nickname)  # Solicita a chave pública do destinatário ao servidor se ainda não estiver armazenada localmente
+                self.request_public_key(destination_nickname)
                 public_key = self.other_public_keys[destination_nickname]
 
-            encrypted_message = rsa.encrypt(message.encode('utf-8'), public_key)  # Criptografa a mensagem usando a chave pública do destinatário
-            encoded_ciphertext = base64.b64encode(encrypted_message).decode('utf-8')  # Codifica a mensagem criptografada em base64
+            # Criptografa a mensagem com a chave pública do destinatário
+            encrypted_message = rsa.encrypt(message.encode('utf-8'), public_key)
+            encoded_ciphertext = base64.b64encode(encrypted_message).decode('utf-8')
 
-            full_message = f'{destination_nickname}:{encoded_ciphertext}'  # Formata a mensagem completa com o apelido do destinatário e a mensagem criptografada
-            full_message_size_data = len(full_message).to_bytes(4, byteorder='big')  # Obtém o tamanho da mensagem completa em bytes
+            # Envia a mensagem criptografada para o servidor com o apelido do destinatário
+            full_message = f'{destination_nickname}:{encoded_ciphertext}'
+            full_message_size_data = len(full_message).to_bytes(4, byteorder='big')
 
-            self.client.send(full_message_size_data + full_message.encode('utf-8'))  # Envia o tamanho e a mensagem completa para o servidor
+            self.client.send(full_message_size_data + full_message.encode('utf-8'))
 
         except Exception as e:
             print(e)
 
+    # Solicita a chave pública de outro usuário ao servidor
     def request_public_key(self, destination_nickname):
-        request_message = f'PUBLIC_KEY_REQUEST:{destination_nickname}'  # Formata a mensagem de solicitação de chave pública com o apelido do destinatário
-        request_message_size_data = len(request_message).to_bytes(4, byteorder='big')  # Obtém o tamanho da mensagem de solicitação em bytes
-        self.client.send(request_message_size_data + request_message.encode('utf-8'))  # Envia o tamanho e a mensagem de solicitação para o servidor
+        request_message = f'PUBLIC_KEY_REQUEST:{destination_nickname}'
+        request_message_size_data = len(request_message).to_bytes(4, byteorder='big')
+        self.client.send(request_message_size_data + request_message.encode('utf-8'))
 
+    # Recebe mensagens do servidor e as descriptografa se necessário
     def receive_messages(self):
+        # Recebe as mensagens continuamente
         while True:
             try:
-                received_message_size_data = self.client.recv(4)  # Recebe o tamanho da mensagem recebida do servidor em bytes
-                received_message_size = int.from_bytes(received_message_size_data, byteorder='big')  # Converte o tamanho da mensagem recebida para um inteiro
-
-                received_message = self.client.recv(received_message_size)  # Recebe a mensagem completa do servidor
+                received_message_size_data = self.client.recv(4)
+                received_message_size = int.from_bytes(received_message_size_data, byteorder='big')
+                # Recebe a mensagem do servidor com o tamanho especificado
+                received_message = self.client.recv(received_message_size)
+                
+                # Decodifica a mensagem recebida
                 str_received_message = received_message.decode('unicode_escape')
 
                 if ':' in str_received_message:
-                    nickname_requested, encrypted_message = str_received_message.split(':', 1)  # Separa o apelido do remetente e a mensagem criptografada
+                
+                    # Separa a mensagem recebida em apelido e mensagem criptografada
+                    nickname_requested, encrypted_message = str_received_message.split(':', 1)
 
+                    # Se a mensagem contém a string '-BEGIN RSA PUBLIC KEY-' está recebendo uma chave publica solicitada
                     if '-BEGIN RSA PUBLIC KEY-' in encrypted_message:                        
-                        encrypted_message = encrypted_message[2:]
-                        public_key = rsa.PublicKey.load_pkcs1(encrypted_message)  # Carrega a chave pública do remetente em formato PKCS1
-                        self.other_public_keys[nickname_requested] = public_key  # Armazena a chave pública do remetente localmente
+                        # Carrega a chave pública do usuário que enviou a mensagem e
+                        # Armazena a chave pública do usuário que enviou a mensagem no dicionário de chaves públicas
+                        encrypted_message = encrypted_message[2:]                        
+                        public_key = rsa.PublicKey.load_pkcs1(encrypted_message)
+                        self.other_public_keys[nickname_requested] = public_key
                         print(f'\nChave Pública de {nickname_requested} recebida.\n Pronto para enviar mensagens para {nickname_requested}.\n')
                     else:
+                                            # Se a mensagem criptografada não contém a string '-BEGIN RSA PUBLIC KEY-', então é uma mensagem criptografada normal
                         sender_nickname = nickname_requested
-                        encrypted_message = base64.b64decode(encrypted_message.encode('utf-8'))  # Decodifica a mensagem criptografada de base64
-                        decrypted_message = rsa.decrypt(encrypted_message, self.private_key).decode('utf-8')  # Descriptografa a mensagem usando a chave privada do usuário
+                        # Decodifica a mensagem criptografada usando base64
+                        encrypted_message = base64.b64decode(encrypted_message.encode('utf-8'))
+                        # Descriptografa a mensagem usando a chave privada do cliente
+                        decrypted_message = rsa.decrypt(encrypted_message, self.private_key).decode('utf-8')
+                        # Imprime a mensagem descriptografada
                         print(f'\nMensagem recebida: {decrypted_message}')
                 else:
                     print(f"{'Usuario nao identificado. Falha'}")
             except rsa.DecryptionError as e:
                 print('\nMensagem recebida nao pode ser descriptograda.\n')
 
+
 if __name__ == '__main__':
-    nickname = input('Digite seu apelido: ')  # Pede ao usuário para digitar seu apelido
-    client = Client('localhost', 5050, nickname)  # Cria um objeto de cliente com o endereço IP e a porta do servidor e o apelido do usuário
-    client.connect()  # Conecta-se ao servidor de chat
+    # Solicita ao usuário que insira seu apelido
+    nickname = input('Digite seu apelido: ')
+    # Cria um novo objeto cliente com o apelido fornecido
+    client = Client('localhost', 5050, nickname)
+    # Conecta o cliente ao servidor
+    client.connect()
 
-    receive_thread = threading.Thread(target=client.receive_messages)  # Cria uma thread para receber mensagens do servidor
-    receive_thread.start()  # Inicia a thread de recebimento de mensagens
+    # Inicia uma nova thread para receber mensagens do servidor
+    receive_thread = threading.Thread(target=client.receive_messages)
+    receive_thread.start()
 
+    # Continuamente solicita ao usuário que insira um apelido de destino e uma mensagem
     while True:
-        destination_nickname = input('Digite o apelido do destinatário:')  # Pede ao usuário para digitar o apelido do destinatário
-        message = input("Digite sua mensagem ou 'sair' para finalizar:")  # Pede ao usuário para digitar sua mensagem ou 'sair' para finalizar
+        destination_nickname = input('Digite o apelido do destinatário:')
+        message = input("Digite sua mensagem ou 'sair' para finalizar:")
 
-        if message.lower() == "sair":  # Se o usuário digitar 'sair'
-            client.disconnect()  # Desconecta-se do servidor
-            receive_thread.join()  # Aguarda a thread de recebimento de mensagens finalizar
-            break  # Sai do loop
+        # Se a mensagem for "sair", desconecta o cliente e encerra a thread de recebimento de mensagens
+        if message.lower() == "sair":
+            client.disconnect()
+            receive_thread.join()
+            break
 
-        send_thread = threading.Thread(target=client.send_message, args=(message, destination_nickname))  # Cria uma thread para enviar a mensagem para o destinatário
-        send_thread.start()  # Inicia a thread de envio de mensagens
+        # Inicia uma nova thread para enviar a mensagem para o destinatário especificado
+        send_thread = threading.Thread(target=client.send_message, args=(message, destination_nickname))
+        send_thread.start()
--- servidor.py
+++ servidor.py
@@ -1,33 +1,40 @@
 import socket
 import threading
 import rsa
-import base64
 
 class Server:
     def __init__(self, host, port):
+        # Inicializa o servidor com o endereço e a porta especificados
         self.host = host
         self.port = port
+        # Cria um novo objeto de soquete para o servidor
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Associa o soquete ao endereço e à porta especificados
         self.server.bind((self.host, self.port))
+        # Coloca o servidor em modo de escuta para aceitar conexões de entrada
         self.server.listen(5)
+        # Inicializa uma lista vazia para armazenar os clientes conectados
         self.clients = []
+        # Inicializa um dicionário vazio para armazenar as chaves públicas dos clientes
         self.public_keys = {}
+        # Inicializa um dicionário vazio para armazenar os apelidos dos clientes
         self.nicknames = {}
 
     def handle_client(self, client, nickname_client):
         while True:
-            # Receber o tamanho da mensagem criptografada em bytes
+            # Recebe o tamanho da mensagem criptografada em bytes do cliente
             encrypted_message_size_data = client.recv(4)
             encrypted_message_size = int.from_bytes(encrypted_message_size_data, byteorder='big')
 
-
-            # Receber a mensagem criptografada
+            # Recebe a mensagem criptografada do cliente
             encrypted_message = client.recv(encrypted_message_size)
 
+            # Decodifica a mensagem criptografada
             message = encrypted_message.decode('utf-8')
             if message.startswith('PUBLIC_KEY_REQUEST:'):
-                # A mensagem é uma solicitação de chave pública
+                # A mensagem é uma solicitação de chave pública de outro usuário
                 nickname_key_requested = message.split(':', 1)[1]
+                # Envia a chave pública do usuário solicitado ao cliente
                 self.send_public_key(client, nickname_key_requested)
                 print(f"Solicitando chave publica de {nickname_client}")
             else:
@@ -36,28 +43,29 @@ class Server:
                     destination_nickname, message = message.split(':', 1)
 
                     if destination_nickname in self.public_keys:
+                        # O usuário de destino está conectado ao servidor
 
-                        # Enviar o tamanho da mensagem criptografada em bytes
+                        # Envia o tamanho da mensagem criptografada em bytes ao usuário de destino
                         encrypted_message_size_data = len(encrypted_message).to_bytes(4, byteorder='big')
 
-                        
-                        # Enviar a mensagem criptografada ao usuário de destino
+                        # Envia a mensagem criptografada ao usuário de destino
                         destination_client = self.nicknames[destination_nickname]
                         destination_client.send(encrypted_message_size_data + encrypted_message)
                     else:
-                        # Usuário de destino não encontrado
+                        # Usuário de destino não encontrado ou não conectado ao servidor
                         error_message = f'ERROR: Usuário {destination_nickname} não encontrado'
                         client.send(error_message.encode('utf-8'))
                 else:
-                    # A mensagem não contém um nome de usuário de destino
+                    # A mensagem não contém um nome de usuário de destino e deve ser transmitida para todos os usuários conectados
                     self.broadcast(client, message.encode('utf-8'))
 
-    def send_public_key(self, client, nickname_key_requested,):
+
+    def send_public_key(self, client, nickname_key_requested):
         if nickname_key_requested in self.public_keys:
-            # Enviar a chave pública do remetente ao cliente solicitante
+            # Envia a chave pública do remetente ao cliente solicitante
             print(f"Enviando chave de {nickname_key_requested}")
             public_key_data = self.public_keys[nickname_key_requested].save_pkcs1()
-            full_message_public_key = f'{nickname_key_requested}:{public_key_data}'            
+            full_message_public_key = f'{nickname_key_requested}:{public_key_data}'
 
             public_key_size_data = len(full_message_public_key).to_bytes(4, byteorder='big')
             client.send(public_key_size_data + full_message_public_key.encode('utf-8'))
@@ -67,33 +75,46 @@ class Server:
             client.send(error_message.encode('utf-8'))
 
     def receive_public_key(self, client):
-        # Receber o tamanho da chave pública em bytes
+        # Recebe o tamanho da chave pública em bytes do cliente
         public_key_size_data = client.recv(4)
         public_key_size = int.from_bytes(public_key_size_data, byteorder='big')
 
-        # Receber a chave pública
+        # Recebe a chave pública do cliente
         public_key_data = client.recv(public_key_size)
         public_key = rsa.PublicKey.load_pkcs1(public_key_data)
 
-        # Receber o apelido
+        # Recebe o apelido do cliente
         nickname = client.recv(1024).decode('utf-8')
+        # Armazena a chave pública e o objeto de soquete do cliente nos dicionários apropriados
         self.public_keys[nickname] = public_key
         self.nicknames[nickname] = client
         print(f'Chave pública de {nickname} recebida e armazenada!')
         return nickname
 
-    def start(self):
-        print('Servidor aguardando conexões')
-        while True:
-            client, address = self.server.accept()
-            print(f'Conectado com {str(address)}')
-            nickname = self.receive_public_key(client)
-            self.clients.append(client)
-            print(f'Nickname do cliente: {nickname}')
-            thread = threading.Thread(target=self.handle_client, args=(client, nickname))
-            thread.start()
+    def broadcast(self, sender_client, message):
+        # Envia a mensagem para todos os clientes conectados, exceto o remetente
+        for client in self.clients:
+            if client != sender_client:
+                client.send(message)
+    
+def start(self):
+    # Coloca o servidor em modo de escuta para aceitar conexões de entrada
+    print('Servidor aguardando conexões')
+    while True:
+        # Aceita uma nova conexão de entrada
+        client, address = self.server.accept()
+        print(f'Conectado com {str(address)}')
+        # Recebe a chave pública e o apelido do cliente
+        nickname = self.receive_public_key(client)
+        # Adiciona o objeto de soquete do cliente à lista de clientes conectados
+        self.clients.append(client)
+        print(f'Nickname do cliente: {nickname}')
+        # Inicia uma nova thread para lidar com as mensagens recebidas do cliente
+        thread = threading.Thread(target=self.handle_client, args=(client, nickname))
+        thread.start()
 
 
 if __name__ == '__main__':
+    # Cria uma instância da classe Server e inicia o servidor
     server = Server('localhost', 5050)
     server.start()
